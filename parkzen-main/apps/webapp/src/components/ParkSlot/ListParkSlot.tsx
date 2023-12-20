b924962157e8f02022fe1e530652706512bf0d46
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2023-12-20 22:02:43 +0100

    chore(webapp): Récupération et affichage de tous les parkslots

--- parkzen-main/apps/webapp/src/components/ParkSlot/ListParkSlot.tsx
+++ parkzen-main/apps/webapp/src/components/ParkSlot/ListParkSlot.tsx
@@ -1,24 +1,63 @@
 import { Table } from "react-bootstrap";
+import {useEffect, useState} from "react";
+
+const apiUrl = process.env.REACT_APP_API_HOST;
+
+interface ParkSlot {
+    _id: number;
+    id: string;
+    firstName: string;
+    lastName: string;
+    priceForOneHour: number;
+    createdAt: Date;
+}
 
 function ListParkSlot() {
-  return (
-    <Table striped bordered>
-      <thead>
-        <tr>
-          <th>Prénom</th>
-          <th>Nom</th>
-          <th>Prix pour une heure</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Alexis</td>
-          <td>Bonal</td>
-          <td>23</td>
-        </tr>
-      </tbody>
-    </Table>
-  );
+    const [parkSlots, setParkSlots] = useState<ParkSlot[]>([]);
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch(`${apiUrl}/parks`)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('La requête n\'a pas abouti');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    setParkSlots(data);
+                })
+                .catch((error) => {
+                    console.error('Erreur lors de la récupération des ParkSlots :', error);
+                });
+        };
+
+        fetchData();
+    }, []);
+
+
+    return (
+        <Table striped bordered>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Prix pour une heure</th>
+            </tr>
+            </thead>
+            <tbody>
+            {parkSlots.map((parkSlot) => (
+                <tr key={parkSlot.id}>
+                    <td>{parkSlot.id}</td>
+                    <td>{parkSlot.firstName}</td>
+                    <td>{parkSlot.lastName}</td>
+                    <td>{parkSlot.priceForOneHour}</td>
+                </tr>
+            ))}
+            </tbody>
+        </Table>
+    );
 }
 
 export default ListParkSlot;
